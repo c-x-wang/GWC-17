bd40d8cc57d271f1bd3e806b8a3373c46d7f412f
Cindy Wang <cindywang.222@gmail.com>
2017-07-06 13:59:43 -0700

    Create flashing-LEDs.ino

---
+++ 0706-Thursday/flashing-LEDs.ino
@@ -0,0 +1,11 @@
+// flashing 2 red and 1 blue in middle 
+for (int i = 0; i < 16; i++) {
+  digitalWrite(11, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(7, HIGH);
+  delay(500);
+  digitalWrite(11, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(7, LOW);
+  delay(500);
+}
