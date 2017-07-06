f2d1806578164099f1f36ece1607391129227cc0
Cindy Wang <cindywang.222@gmail.com>
2017-07-06 14:02:01 -0700

    Update flashing-LEDs.ino

--- 0706-Thursday/flashing-LEDs.ino
+++ 0706-Thursday/flashing-LEDs.ino
@@ -3,9 +3,9 @@ for (int i = 0; i < 16; i++) {
   digitalWrite(11, HIGH);
   digitalWrite(9, HIGH);
   digitalWrite(7, HIGH);
-  delay(500);
+  delay(437.5);
   digitalWrite(11, LOW);
   digitalWrite(9, LOW);
   digitalWrite(7, LOW);
-  delay(500);
+  delay(437.5);
 }
